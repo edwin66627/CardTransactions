a054af5757dca1ee94327fbfbf3067d04bc9b106
Edwin Erley Quevedo Sierra <edwinqs1956@hotmail.com>
2023-08-23 16:36:53 -0500

    Added User-related repositories

---
+++ src/main/java/com/nexus/repository/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.nexus.repository;
+
+import com.nexus.entity.Employee;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends UserRepository {
+}
